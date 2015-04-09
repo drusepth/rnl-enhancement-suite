1b7dc64eed219365c2764b7cd78aabbb2bfaaaa3
Andrew Brown <abrown@rednovalabs.com>
2015-04-08 21:03:04 -0500

    Code cleanup + drilldown release status bar

--- src/js/jira.js
+++ src/js/jira.js
@@ -1,22 +1,97 @@
-$(document).ready(function () {
-    console.log('loaded jira');
+console.log('loaded jira');
 
-    $('div#release-report-tab-body').bind("DOMSubtreeModified", function() {
-        // Apply a red color to rejected lozenges
-        $(this).find('.jira-issue-status-lozenge-yellow:contains("Rejected")')
-            .removeClass('jira-issue-status-lozenge-yellow')
-            .addClass('jira-issue-status-lozenge-warm-red');
+var status_colors = {
+    'to do': 'blue-gray',
+    'analyze': 'blue-gray',
+    'rejected': 'warm-red',
+    'in progress': 'yellow',
+    'demo': 'yellow',
+    'needs review': 'yellow',
+    'qa ready': 'medium-gray',
+    'qa in progress': 'medium-gray',
+    'staging ready': 'yellow',
+    'on staging': 'medium-gray',
+    'staging qa': 'medium-gray',
+    'staging rejected': 'warm-red',
+    'accepted for release': 'green',
+    'done': 'green'
+};
 
-        // Apply a brown color for QA lozenges
-        $(this).find('.jira-issue-status-lozenge-yellow')
-            .filter(function () {
-                var lozenge_text = $(this).text().toLowerCase();
-                return lozenge_text.indexOf('qa') > -1 || lozenge_text.indexOf('staging') > -1;
-            })
-            .removeClass('jira-issue-status-lozenge-yellow')
-            .addClass('jira-issue-status-lozenge-medium-gray');
+function eval_when_present(selector, block) {
+    var watcher = setInterval(function () {
+        if ($(selector)) {
+            block();
+            clearInterval(watcher);
+        }
+    }, 200);
+}
+
+function issues_in_status(desired_status) {
+    return $('tr[data-issue-key]').filter(function () {
+        return desired_status.toLowerCase() === $(this).find('td.status > span.jira-issue-status-lozenge').text().toLowerCase();
+    });
+}
+
+$('nav.release-report-tab-header').on('click', '.release-report-tab', function() {
+    eval_when_present('.release-report-issues', function () {
+
+        // Recolor status lozenges
+        $.each(status_colors, function (status, color) {
+            issues_in_status(status)
+                .find('.jira-issue-status-lozenge')
+                .removeClass('jira-issue-status-lozenge-yellow') // #todo todo/done don't have yellow default
+                .addClass('jira-issue-status-lozenge-' + color);
+        });
+
+        // Redraw release status bar
+        var release_bar = $('div.aui-group.status-category-blocks');
+        if (release_bar) {
+            var issue_count_by_status = {},
+                issue_count_by_color = {},
+                total_issues = 0;
+
+            $.each(status_colors, function (status, color) {
+                var issue_count = issues_in_status(status).length;
+                issue_count_by_status[status] = issue_count;
+                if (color in issue_count_by_color) {
+                    issue_count_by_color[color] += issue_count;
+                } else {
+                    issue_count_by_color[color] = issue_count;
+                }
+                total_issues += issue_count;
+            });
+
+            var segment_template = release_bar.find('div.aui-item')
+                .first()
+                .removeAttr('style')
+                .removeClass('green')
+                .removeClass('yellow')
+                .removeClass('blue-gray')
+                .removeClass('done')
+                .removeClass('intermediate')
+                .removeClass('new');
+
+            release_bar.html('');
+            $.each(issue_count_by_color, function (color, number) {
+                console.log('adding segment for ' + color + ' size ' + number);
+                if (number > 0) {
+                    segment_template.clone()
+                        .addClass(color)
+                        .css('width', number / total_issues * 100 + '%')
+                        .css('height', '20')
+                        .css('color', '#ffffff')
+                        .css('text-align', 'center')
+                        .css('overflow', 'hidden')
+                        .text(number + ' issue' + (number == 1 ? '' : 's'))
+                        .appendTo(release_bar);
+                }
+            });
+        }
 
     });
-
-    console.log('done applying jira');
 });
+
+
+
+
+console.log('done applying jira');
